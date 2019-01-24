3c73e10be3a66e7b88f4d70ea4e7f17c14493abf
April Dagonese <adagonese@gmail.com>
2019-01-24 11:57:49 -0700

    Adds more test setup

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :playlist
+
+  def self.total_play_count
+    sum(:play_count)
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+ENV["RACK_ENV"] = "test"
 require 'bundler'
 Bundler.require(:default, :test)
 require File.expand_path('../../config/environment.rb', __FILE__)

224a1df6c13e8cfd8bbbdf012ddb65858e20ea3f
April Dagonese <adagonese@gmail.com>
2019-01-23 18:26:55 -0700

    Adds pry; adds songs/:id route

--- app/controllers/set_list_app.rb
+++ app/controllers/set_list_app.rb
@@ -2,5 +2,11 @@ class SetList < Sinatra::Base
   get '/songs' do
     @songs = Song.all
     erb :"songs/index"
-  end 
+  end
+
+  get '/songs/:id' do
+    require 'pry' ; binding.pry
+    @song = Song.find(params[:id])
+    erb :"songs/show"
+  end
 end
